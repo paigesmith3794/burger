76d0d11085142d3fd58d2e51582c16ac499a807b
paigesmith3794 <paigesmith3794@gmail.com>
2019-06-21 15:45:56 -0700

    seeds and schema files created

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Melted Cheese Madness');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Buffalo Burger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Bacon Burger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Massive Burger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Veggie Burger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Vegan Beyond Burger');
